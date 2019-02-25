279cfaef26b99f9fe535de0beb9b5bdbb3efa7e8
Bruce 🛳  Gou <brucegou@gmail.com>
2019-02-25 00:19:40 -0800

    show number of items

--- src/menu/OrderPanel.js
+++ src/menu/OrderPanel.js
@@ -5,6 +5,17 @@ import Course from './Course'
 import range from 'lodash/range'
 import cloneDeep from 'lodash/cloneDeep'
 
+const getCartId = (menuItem, index) => {
+  return `${menuItem.id}-${index}`
+}
+
+const createNewItem = (menuItem, i) => {
+  return {
+    id: getCartId(menuItem.id, i),
+    menuItem: cloneDeep(menuItem),
+  }
+}
+
 // Keeps track of how many orders each item has
 // Each item may have different choices for different categories
 export class OrderPanel extends Component {
@@ -13,15 +24,55 @@ export class OrderPanel extends Component {
     qty: PropTypes.number,
   }
 
-  render() {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [],
+    }
+  }
+
+  componentWillMount() {
     const { menuItem, qty } = this.props
+
     const items = range(qty).map(i => {
-      const id = `${menuItem.id}-${qty}-${i}`
-      return {
-        id,
-        menuItem: cloneDeep(menuItem),
-      }
+      return createNewItem(menuItem, i)
     })
+
+    this.setState({ items })
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.qty !== prevProps.qty) {
+      this.onQuantityChange(this.props.qty)
+    }
+  }
+
+  // when quantity is changed from outside
+  onQuantityChange = newQty => {
+    const { menuItem } = this.props
+
+    const oldQty = this.state.items.length
+
+    if (newQty > oldQty) {
+      // add new item(s)
+      const addCount = newQty - oldQty
+      const newItems = range(addCount).map(i => {
+        return createNewItem(menuItem, oldQty + i)
+      })
+      this.setState({
+        items: [...this.state.items, ...newItems],
+      })
+    } else if (oldQty > newQty) {
+      // remove last item
+      this.setState({
+        items: [...this.state.items.slice(0, newQty)],
+      })
+    }
+    // update states
+  }
+
+  render() {
+    const { items } = this.state
 
     return (
       <Grid container>
