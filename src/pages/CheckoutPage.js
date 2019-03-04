ae02010ee0207a126655da26363ce56942935848
Bruce 🛳  Gou <brucegou@gmail.com>
2019-03-03 23:58:31 -0800

    Clicking back on cart navigates to main page

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,10 +1,12 @@
 import Button from '@material-ui/core/Button'
 import Icon from '@material-ui/core/Icon'
 import { withStyles } from '@material-ui/core/styles'
-import React from 'react'
+import React, { Component } from 'react'
 import Cart from '../checkout/Cart'
 import Delivery from '../checkout/Delivery'
 import Payment from '../checkout/Payment'
+import { withRouter } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
 const styles = theme => ({
   root: {
@@ -21,23 +23,42 @@ const styles = theme => ({
   },
 })
 
-const CheckoutPage = ({ classes }) => {
-  return (
-    <div className={classes.root}>
-      <Button fullWidth variant="outlined" className={classes.backButton}>
-        <Icon className={classes.leftIcon}>arrow_left</Icon>
-        Back
-      </Button>
-      <Cart />
-      <Delivery />
-      <Payment />
-      <Button fullWidth variant="contained" color="primary">
-        <Icon className={classes.leftIcon}>pets</Icon>
-        Order
-        <Icon className={classes.rightIcon}>pets</Icon>
-      </Button>
-    </div>
-  )
+export class CheckoutPage extends Component {
+  static propTypes = {
+    classes: PropTypes.object.isRequired,
+    match: PropTypes.object.isRequired,
+    location: PropTypes.object.isRequired,
+    history: PropTypes.object.isRequired,
+  }
+
+  goBack = () => {
+    this.props.history.push('/')
+  }
+
+  render() {
+    const { classes } = this.props
+    return (
+      <div className={classes.root}>
+        <Button
+          fullWidth
+          variant="outlined"
+          className={classes.backButton}
+          onClick={this.goBack.bind(this)}
+        >
+          <Icon className={classes.leftIcon}>arrow_left</Icon>
+          Back
+        </Button>
+        <Cart />
+        <Delivery />
+        <Payment />
+        <Button fullWidth variant="contained" color="primary">
+          <Icon className={classes.leftIcon}>pets</Icon>
+          Order
+          <Icon className={classes.rightIcon}>pets</Icon>
+        </Button>
+      </div>
+    )
+  }
 }
 
-export default withStyles(styles)(CheckoutPage)
+export default withRouter(withStyles(styles)(CheckoutPage))
